58becffc1abd8280d5798d19a175e4d9ddb5f534
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-02-09 13:51:43 -0600

    added ModelCheckpoint

--- BMA_Callbacks.py
+++ BMA_Callbacks.py
@@ -1,6 +1,7 @@
 # Please note reference to call back in CyclicLR_onecycle, this has been slightly adapted from the original model
 import numpy as np
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import Callback
+import warnings
 
 
 def dice_coef_3D(y_true, y_pred, smooth=0.0001):
@@ -8,16 +9,17 @@ def dice_coef_3D(y_true, y_pred, smooth=0.0001):
     union = np.sum(y_true[...,1:]) + np.sum(y_pred[...,1:])
     return (2. * intersection + smooth) / (union + smooth)
 
-class ModelCheckpoint_new(ModelCheckpoint):
+class ModelCheckpoint_new(Callback):
 
     def __init__(self, filepath, monitor='val_loss', verbose=0,
                  save_best_only=False, save_weights_only=False,
-                 mode='auto', period=1, model=None):
+                 mode='auto', period=1, model=None, save_best_and_all=False):
         self.is_gpu_model = False
         if model:
             self.save_model = model
             self.is_gpu_model = True
-        super(ModelCheckpoint, self).__init__()
+        super(ModelCheckpoint_new, self).__init__()
+        self.save_best_and_all = save_best_and_all
         self.monitor = monitor
         self.verbose = verbose
         self.filepath = filepath
@@ -55,7 +57,8 @@ class ModelCheckpoint_new(ModelCheckpoint):
         self.epochs_since_last_save += 1
         if self.epochs_since_last_save >= self.period:
             filepath = self.filepath.replace('{epoch:02d}',str(epoch + 1))
-            if self.save_best_only:
+            if self.save_best_only or self.save_best_and_all:
+                filepath_best = filepath.replace('.hdf5','_best.hdf5')
                 current = logs.get(self.monitor)
                 if current is None:
                     warnings.warn('Can save best model only with %s available, '
@@ -66,17 +69,17 @@ class ModelCheckpoint_new(ModelCheckpoint):
                             print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
                                   ' saving model to %s'
                                   % (epoch + 1, self.monitor, self.best,
-                                     current, filepath))
+                                     current, filepath_best))
                         self.best = current
                         if self.save_weights_only:
-                            self.model.save_weights(filepath, overwrite=True)
+                            self.model.save_weights(filepath_best, overwrite=True)
                         else:
-                            self.model.save(filepath, overwrite=True)
+                            self.model.save(filepath_best, overwrite=True)
                     else:
                         if self.verbose > 0:
                             print('\nEpoch %05d: %s did not improve from %0.5f' %
                                   (epoch + 1, self.monitor, self.best))
-            else:
+            if not self.save_best_only or self.save_best_and_all:
                 self.epochs_since_last_save = 0
                 if self.verbose > 0:
                     print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
