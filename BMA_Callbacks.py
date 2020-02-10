6c19741f7fb8caae041ca900a3eb2ca5b594662b
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-02-09 21:03:37 -0600

    callback to get the learning rate

--- BMA_Callbacks.py
+++ BMA_Callbacks.py
@@ -2,6 +2,7 @@
 import numpy as np
 from keras.callbacks import Callback
 import warnings
+from keras.backend import get_value
 
 
 def dice_coef_3D(y_true, y_pred, smooth=0.0001):
@@ -47,6 +48,7 @@ class ModelCheckpoint_new(Callback):
             else:
                 self.monitor_op = np.less
                 self.best = np.Inf
+
     def set_path(self, path):
         self.path = path
 
@@ -55,30 +57,30 @@ class ModelCheckpoint_new(Callback):
             self.save_model = self.model
         self.epoch = epoch + 1
         self.epochs_since_last_save += 1
-        if self.epochs_since_last_save >= self.period:
-            filepath = self.filepath.replace('{epoch:02d}',str(epoch + 1))
-            if self.save_best_only or self.save_best_and_all:
-                filepath_best = filepath.replace('.hdf5','_best.hdf5')
-                current = logs.get(self.monitor)
-                if current is None:
-                    warnings.warn('Can save best model only with %s available, '
-                                  'skipping.' % (self.monitor), RuntimeWarning)
-                else:
-                    if self.monitor_op(current, self.best):
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
-                                  ' saving model to %s'
-                                  % (epoch + 1, self.monitor, self.best,
-                                     current, filepath_best))
-                        self.best = current
-                        if self.save_weights_only:
-                            self.model.save_weights(filepath_best, overwrite=True)
-                        else:
-                            self.model.save(filepath_best, overwrite=True)
+        filepath = self.filepath.replace('{epoch:02d}', str(epoch + 1))
+        if self.save_best_only or self.save_best_and_all:
+            filepath_best = filepath.replace('.hdf5', '_best.hdf5')
+            current = logs.get(self.monitor)
+            if current is None:
+                warnings.warn('Can save best model only with %s available, '
+                              'skipping.' % (self.monitor), RuntimeWarning)
+            else:
+                if self.monitor_op(current, self.best):
+                    if self.verbose > 0:
+                        print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                              ' saving model to %s'
+                              % (epoch + 1, self.monitor, self.best,
+                                 current, filepath_best))
+                    self.best = current
+                    if self.save_weights_only:
+                        self.model.save_weights(filepath_best, overwrite=True)
                     else:
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s did not improve from %0.5f' %
-                                  (epoch + 1, self.monitor, self.best))
+                        self.model.save(filepath_best, overwrite=True)
+                else:
+                    if self.verbose > 0:
+                        print('\nEpoch %05d: %s did not improve from %0.5f' %
+                              (epoch + 1, self.monitor, self.best))
+        if self.epochs_since_last_save >= self.period:
             if not self.save_best_only or self.save_best_and_all:
                 self.epochs_since_last_save = 0
                 if self.verbose > 0:
@@ -87,3 +89,10 @@ class ModelCheckpoint_new(Callback):
                     self.save_model.save_weights(filepath, overwrite=True)
                 else:
                     self.save_model.save(filepath, overwrite=True)
+
+
+class Add_LR_To_Tensorboard(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs is not None:
+            logs['learning_rate'] = get_value(self.model.optimizer.lr)
+        return logs
