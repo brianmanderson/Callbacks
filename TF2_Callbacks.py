5a02e42b9671326ed4d9f4ec061ea02649052b61
Anderson <BMAnderson@mdanderson.org>
2020-04-17 10:17:34 -0500

    scale not inside batch...

--- TF2_Callbacks.py
+++ TF2_Callbacks.py
@@ -128,14 +128,15 @@ class Add_Images(tf.keras.callbacks.Callback):
             pred_out = pred[0,...]
             x = x[...,0]
             x, y, pred_out = self.return_proper_size(x), self.return_proper_size(y), self.return_proper_size(pred_out)
+            x, y, pred_out = self.scale_0_1(x), self.scale_0_1(y), self.scale_0_1((pred_out))
             output_x.append(x)
             output_y.append(y)
             output_pred.append(pred_out)
         x, y, pred_out = tf.concat(output_x, axis=2), tf.concat(output_y, axis=2), tf.concat(output_pred, axis=2)
         with self.file_writer.as_default():
-            tf.summary.image('Image', tf.cast(self.scale_0_1(x), 'float32'), step=epoch)
-            tf.summary.image('Truth', tf.cast(self.scale_0_1(x), 'float32'), step=epoch)
-            tf.summary.image('Pred', tf.cast(self.scale_0_1(x), 'float32'), step=epoch)
+            tf.summary.image('Image', tf.cast(x, 'float32'), step=epoch)
+            tf.summary.image('Truth', tf.cast(y, 'float32'), step=epoch)
+            tf.summary.image('Pred', tf.cast(pred_out, 'float32'), step=epoch)
 
 
 if __name__ == '__main__':
