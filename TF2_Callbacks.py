641e12b603caadca35a3e6324dd994db27c1b49e
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-04-27 17:05:21 -0500

    should always be passing inputs and outptus as tuples now, so just updating

--- TF2_Callbacks.py
+++ TF2_Callbacks.py
@@ -1,5 +1,4 @@
 __author__ = 'Brian M Anderson'
-
 # Created on 4/15/2020
 import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
@@ -28,34 +27,44 @@ class Add_Images_and_LR(Callback):
         val = tf.divide(val, tf.reduce_max(val))
         return val
 
-    def write_images(self, epoch):
+    def write_images(self):
         output_x = []
         output_y = []
         output_pred = []
+        print('Writing out images')
         for i in range(self.number_of_images):
-            x, y = next(self.validation_data)
-            y = y[0]
-            y = tf.squeeze(y)
+            print('Writing out image {}'.format(i))
+            x, y_base = next(self.validation_data)
+            y = tf.squeeze(y_base[0])
             indexes = tf.unique(tf.where(y > 0)[..., 0])[0]
             index = indexes[tf.shape(indexes)[0] // 2]
-            x = tf.expand_dims(x[index,...],axis=0)
-            y = y[index, ...]
-            pred = self.model(x, training=False)
-            pred = tf.argmax(pred, axis=-1)
-            pred_out = pred[0,...]
-            x = x[...,0]
-            x, y, pred_out = self.return_proper_size(x), self.return_proper_size(y), self.return_proper_size(pred_out)
-            x, y, pred_out = tf.cast(x, 'float32'), tf.cast(y, 'float32'), tf.cast(pred_out, 'float32')
-            x, y, pred_out = self.scale_0_1(x), self.scale_0_1(y), self.scale_0_1(pred_out)
+            x = x[0]
+            x = tf.expand_dims(x[index, ...], axis=0)
+            pred_base = self.model(x, training=False)
+            x = tf.squeeze(x)
+            if len(x.shape) > 2:
+                x = x[..., -1]
+            x = self.scale_0_1(tf.cast(self.return_proper_size(x), 'float32'))
+            temp_y = []
+            temp_pred = []
+            for val in range(len(y_base)):
+                y = tf.squeeze(y_base[val][index,...])
+                pred = pred_base[val]
+                pred = tf.squeeze(tf.argmax(pred, axis=-1))
+                pred = self.scale_0_1(tf.cast(self.return_proper_size(pred),'float32'))
+                temp_pred.append(pred)
+                temp_y.append(self.scale_0_1(tf.cast(self.return_proper_size(y),'float32')))
+            pred_out = tf.concat(temp_pred, axis=1)
+            y_out = tf.concat(temp_y, axis=1)
             output_x.append(x)
-            output_y.append(y)
+            output_y.append(y_out)
             output_pred.append(pred_out)
         x, y, pred_out = tf.concat(output_x, axis=2), tf.concat(output_y, axis=2), tf.concat(output_pred, axis=2)
         return x, y, pred_out
 
     def on_epoch_end(self, epoch, logs=None):
         if self.add_images:
-            x, y, pred_out = self.write_images(epoch=epoch)
+            x, y, pred_out = self.write_images()
             with self.file_writer.as_default():
                 tf.summary.image('Image', tf.cast(x, 'float32'), step=epoch)
                 tf.summary.image('Truth', tf.cast(y, 'float32'), step=epoch)
