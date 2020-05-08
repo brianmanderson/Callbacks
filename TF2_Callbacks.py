96061357e234e1ed34e8645912183875791b073a
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-05-08 14:18:45 -0500

    can't seem to get the model to load..

--- TF2_Callbacks.py
+++ TF2_Callbacks.py
@@ -149,7 +149,7 @@ class MeanDSC(tf.keras.metrics.MeanIoU):
     '''
     This varies from the original in that we don't care about the background DSC
     '''
-    def __init__(self, num_classes, name=None, dtype=None):
+    def __init__(self, num_classes, name='mean_dsc', dtype=None):
         """Creates a `MeanIoU` instance.
 
         Args:
@@ -195,7 +195,7 @@ class MeanJaccard(tf.keras.metrics.MeanIoU):
     '''
     This varies from the original in that we don't care about the background DSC
     '''
-    def __init__(self, num_classes, name=None, dtype=None):
+    def __init__(self, num_classes, name='mean_jaccard', dtype=None):
         """Creates a `MeanIoU` instance.
 
         Args:
@@ -235,56 +235,6 @@ class MeanJaccard(tf.keras.metrics.MeanIoU):
         config = {'num_classes': self.num_classes}
         base_config = super(MeanJaccard, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
-
-class Base_To_Sparse(tf.keras.metrics.MeanIoU):
-    def __init__(self, num_classes, name=None, dtype=None):
-        """Creates a `MeanIoU` instance.
-
-        Args:
-          num_classes: The possible number of labels the prediction task can have.
-            This value must be provided, since a confusion matrix of dimension =
-            [num_classes, num_classes] will be allocated.
-          name: (Optional) string name of the metric instance.
-          dtype: (Optional) data type of the metric result.
-        """
-        super(Base_To_Sparse, self).__init__(num_classes=num_classes, name=name, dtype=dtype)
-
-    def update_state(self, y_true, y_pred, sample_weight=None):
-        """Accumulates the confusion matrix statistics.
-
-        Args:
-          y_true: The ground truth values.
-          y_pred: The predicted values.
-          sample_weight: Optional weighting of each example. Defaults to 1. Can be a
-            `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
-            be broadcastable to `y_true`.
-
-        Returns:
-          Update op.
-        """
-
-        y_true = tf.cast(y_true, self._dtype)
-        y_pred = tf.argmax(y_pred, axis=-1)
-        y_pred = tf.cast(y_pred, self._dtype)
-        # Flatten the input if its rank > 1.
-        if y_pred.shape.ndims > 1:
-            y_pred = tf.reshape(y_pred, [-1])
-
-        if y_true.shape.ndims > 1:
-            y_true = tf.reshape(y_true, [-1])
-
-        if sample_weight is not None and sample_weight.shape.ndims > 1:
-            sample_weight = tf.reshape(sample_weight, [-1])
-
-        # Accumulate the prediction to current confusion matrix.
-        current_cm = confusion_matrix.confusion_matrix(
-            y_true,
-            y_pred,
-            self.num_classes,
-            weights=sample_weight,
-            dtype='float64')
-        return self.total_cm.assign_add(current_cm)
 
 
 class SparseCategoricalMeanDSC(Metric):
@@ -382,11 +332,12 @@ class SparseCategoricalMeanDSC(Metric):
         return dict(list(base_config.items()) + list(config.items()))
 
 
-class SparseCategoricalMeanJaccard(Base_To_Sparse):
+class SparseCategoricalMeanJaccard(Metric):
     '''
     This varies from the original in that we don't care about the background DSC
     '''
-    def __init__(self, num_classes, name=None, dtype=None):
+
+    def __init__(self, num_classes, name='sparse_categorical_mean_jaccard', dtype=None):
         """Creates a `MeanIoU` instance.
 
         Args:
@@ -396,7 +347,52 @@ class SparseCategoricalMeanJaccard(Base_To_Sparse):
           name: (Optional) string name of the metric instance.
           dtype: (Optional) data type of the metric result.
         """
-        super(SparseCategoricalMeanJaccard, self).__init__(num_classes=num_classes, name=name, dtype=dtype)
+        super(SparseCategoricalMeanJaccard, self).__init__(name=name, dtype=dtype)
+        self.num_classes = num_classes
+
+        # Variable to accumulate the predictions in the confusion matrix. Setting
+        # the type to be `float64` as required by confusion_matrix_ops.
+        self.total_cm = self.add_weight(
+            'total_confusion_matrix',
+            shape=(num_classes, num_classes),
+            initializer=init_ops.zeros_initializer,
+            dtype=dtypes.float64)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Accumulates the confusion matrix statistics.
+
+        Args:
+          y_true: The ground truth values.
+          y_pred: The predicted values.
+          sample_weight: Optional weighting of each example. Defaults to 1. Can be a
+            `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
+            be broadcastable to `y_true`.
+
+        Returns:
+          Update op.
+        """
+
+        y_true = tf.cast(y_true, self._dtype)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        y_pred = tf.cast(y_pred, self._dtype)
+        # Flatten the input if its rank > 1.
+        if y_pred.shape.ndims > 1:
+            y_pred = tf.reshape(y_pred, [-1])
+
+        if y_true.shape.ndims > 1:
+            y_true = tf.reshape(y_true, [-1])
+
+        if sample_weight is not None and sample_weight.shape.ndims > 1:
+            sample_weight = tf.reshape(sample_weight, [-1])
+
+        # Accumulate the prediction to current confusion matrix.
+        current_cm = confusion_matrix.confusion_matrix(
+            y_true,
+            y_pred,
+            self.num_classes,
+            weights=sample_weight,
+            dtype='float64')
+        return self.total_cm.assign_add(current_cm)
 
     def result(self):
         """Compute the mean intersection-over-union via the confusion matrix."""
@@ -421,6 +417,9 @@ class SparseCategoricalMeanJaccard(Base_To_Sparse):
 
         jaccard = tf.math.divide_no_nan(tf.reduce_sum(iou), num_valid_entries, name='mean_jaccard')
         return jaccard
+
+    def reset_states(self):
+        tf.keras.backend.set_value(self.total_cm, np.zeros((self.num_classes, self.num_classes)))
 
     def get_config(self):
         config = {'num_classes': self.num_classes}
