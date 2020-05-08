7cd85e6e2391e447d3c494af8ad61bb0a1be2ac3
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-05-08 13:59:40 -0500

    still working on the get_config from_config method

--- TF2_Callbacks.py
+++ TF2_Callbacks.py
@@ -3,6 +3,8 @@ __author__ = 'Brian M Anderson'
 import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
 import os
+import numpy as np
+from tensorflow.keras.metrics import Metric
 from .Plot_And_Scroll_Images.Plot_Scroll_Images import plot_scroll_Image
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import confusion_matrix
@@ -283,10 +285,11 @@ class Base_To_Sparse(tf.keras.metrics.MeanIoU):
         return self.total_cm.assign_add(current_cm)
 
 
-class SparseCategoricalMeanDSC(Base_To_Sparse):
+class SparseCategoricalMeanDSC(Metric):
     '''
     This varies from the original in that we don't care about the background DSC
     '''
+
     def __init__(self, num_classes, name=None, dtype=None):
         """Creates a `MeanIoU` instance.
 
@@ -297,7 +300,52 @@ class SparseCategoricalMeanDSC(Base_To_Sparse):
           name: (Optional) string name of the metric instance.
           dtype: (Optional) data type of the metric result.
         """
-        super(SparseCategoricalMeanDSC, self).__init__(num_classes=num_classes, name=name, dtype=dtype)
+        super(SparseCategoricalMeanDSC, self).__init__(name=name, dtype=dtype)
+        self.num_classes = num_classes
+
+        # Variable to accumulate the predictions in the confusion matrix. Setting
+        # the type to be `float64` as required by confusion_matrix_ops.
+        self.total_cm = self.add_weight(
+            'total_confusion_matrix',
+            shape=(num_classes, num_classes),
+            initializer=init_ops.zeros_initializer,
+            dtype=dtypes.float64)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Accumulates the confusion matrix statistics.
+
+        Args:
+          y_true: The ground truth values.
+          y_pred: The predicted values.
+          sample_weight: Optional weighting of each example. Defaults to 1. Can be a
+            `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
+            be broadcastable to `y_true`.
+
+        Returns:
+          Update op.
+        """
+
+        y_true = tf.cast(y_true, self._dtype)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        y_pred = tf.cast(y_pred, self._dtype)
+        # Flatten the input if its rank > 1.
+        if y_pred.shape.ndims > 1:
+            y_pred = tf.reshape(y_pred, [-1])
+
+        if y_true.shape.ndims > 1:
+            y_true = tf.reshape(y_true, [-1])
+
+        if sample_weight is not None and sample_weight.shape.ndims > 1:
+            sample_weight = tf.reshape(sample_weight, [-1])
+
+        # Accumulate the prediction to current confusion matrix.
+        current_cm = confusion_matrix.confusion_matrix(
+            y_true,
+            y_pred,
+            self.num_classes,
+            weights=sample_weight,
+            dtype='float64')
+        return self.total_cm.assign_add(current_cm)
 
     def result(self):
         """Compute the mean intersection-over-union via the confusion matrix."""
@@ -322,6 +370,9 @@ class SparseCategoricalMeanDSC(Base_To_Sparse):
 
         jaccard = tf.math.divide_no_nan(tf.reduce_sum(iou), num_valid_entries)
         return tf.math.divide_no_nan(tf.multiply(tf.cast(2,'float32'),jaccard),tf.add(tf.cast(1,'float32'),jaccard),name='mean_dsc')
+
+    def reset_states(self):
+        tf.keras.backend.set_value(self.total_cm, np.zeros((self.num_classes, self.num_classes)))
 
     def get_config(self):
         config = {'num_classes': self.num_classes}
