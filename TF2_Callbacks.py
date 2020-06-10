311585c01419f30b23575e8e8089159c94d33693
markba122@gmail.com <bmanderson@mdanderson.org>
2020-06-10 10:15:55 -0500

    updated regular MeanDSC in case we are working with gaussian-smoothed liver lobes, will need argmax for that too

--- TF2_Callbacks.py
+++ TF2_Callbacks.py
@@ -161,6 +161,43 @@ class MeanDSC(tf.keras.metrics.MeanIoU):
         """
         super(MeanDSC, self).__init__(num_classes=num_classes, name=name, dtype=dtype)
 
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Accumulates the confusion matrix statistics.
+
+        Args:
+          y_true: The ground truth values.
+          y_pred: The predicted values.
+          sample_weight: Optional weighting of each example. Defaults to 1. Can be a
+            `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
+            be broadcastable to `y_true`.
+
+        Returns:
+          Update op.
+        """
+
+        y_true = tf.argmax(y_true, axis=-1)
+        y_true = tf.cast(y_true, self._dtype)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        y_pred = tf.cast(y_pred, self._dtype)
+        # Flatten the input if its rank > 1.
+        if y_pred.shape.ndims > 1:
+            y_pred = tf.reshape(y_pred, [-1])
+
+        if y_true.shape.ndims > 1:
+            y_true = tf.reshape(y_true, [-1])
+
+        if sample_weight is not None and sample_weight.shape.ndims > 1:
+            sample_weight = tf.reshape(sample_weight, [-1])
+
+        # Accumulate the prediction to current confusion matrix.
+        current_cm = confusion_matrix.confusion_matrix(
+            y_true,
+            y_pred,
+            self.num_classes,
+            weights=sample_weight,
+            dtype='float64')
+        return self.total_cm.assign_add(current_cm)
+
     def result(self):
         """Compute the mean intersection-over-union via the confusion matrix."""
         sum_over_row = tf.cast(
