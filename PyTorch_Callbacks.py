d75d2ae4205652c039a503f47d119ac5cf63d948
brianmanderson <markba122@gmail.com>
2024-10-28 13:33:38 -0400

    if not applying softmax in model, apply it now

--- PyTorch_Callbacks.py
+++ PyTorch_Callbacks.py
@@ -5,12 +5,15 @@ import torch.nn.functional as F
 
 
 class MeanDSC(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, apply_softmax=False):
         super(MeanDSC, self).__init__()
         self.num_classes = num_classes
         self.eps = 1e-6
+        self.apply_softmax = apply_softmax
 
     def forward(self, y_true, y_pred):
+        if self.apply_softmax:
+            y_pred = F.softmax(y_pred, dim=1)
         y_true = F.one_hot(y_true.argmax(dim=1), num_classes=self.num_classes)
         y_pred = F.one_hot(y_pred.argmax(dim=1), num_classes=self.num_classes)
 
